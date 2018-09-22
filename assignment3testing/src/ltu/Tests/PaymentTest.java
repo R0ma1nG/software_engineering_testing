7213d3d3995ae1d7b8df38373e2cad25d9fa61d9
Victor <victorduvercors@gmail.com>
2018-09-22 11:10:31 +0200

    first tests

--- assignment3testing/src/ltu/Tests/PaymentTest.java
+++ assignment3testing/src/ltu/Tests/PaymentTest.java
@@ -9,6 +9,9 @@ import ltu.PaymentImpl;
 import org.junit.*;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class PaymentTest
 {
@@ -22,10 +25,11 @@ public class PaymentTest
     private final int PARTTIME_INCOME = 128722;
 
     private PaymentImpl payment;
+    private ICalendar calendar;
 
     @Before
     public void init() throws IOException {
-        ICalendar calendar = getCalendar();
+        calendar = getCalendar();
         payment = new PaymentImpl(calendar);
     }
 
@@ -41,6 +45,23 @@ public class PaymentTest
         ICalendar calendar = getCalendar();
         PaymentImpl payment = new PaymentImpl(calendar);
         assertNotNull(payment);
+    }
+
+    @Test
+    public void ageIsValid() throws IOException{
+        Date date = calendar.getDate();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int year = cal.get(Calendar.YEAR);
+        int birthDate = year - 18;
+
+        System.out.println(calendar);
+        System.out.println(calendar.getDate());
+        System.out.println(birthDate);
+
+        //String birthYear = birthDate;
+
+        //assertEquals(0, payment.getMonthlyAmount());
     }
 
 }
